ca97dfcf85e1e2964de50025c37940dde48e7143
katrin0929 <katerin09291999@gmail.com>
2026-07-17 15:30:01 +0300

    position dropdown

--- frontend/src/components/Dropdown.vue
+++ frontend/src/components/Dropdown.vue
@@ -2,12 +2,12 @@
 </script>
 
 <template>
-    <el-dropdown placement="bottom"
+    <el-dropdown placement="bottom-end"
         trigger="click"
         :teleported="true"
     >
         <slot name="trigger-el"></slot>
-        <el-menu anchor="bottom-end" popover="manual" class="w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg outline-1 outline-black/5 transition transition-discrete [--anchor-gap:--spacing(2)] data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in dark:divide-white/10 dark:bg-gray-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10" id="menu-0" role="menu" open="" aria-labelledby="menu-button-1" tabindex="0">
+        <el-menu anchor="bottom-end" popover="manual" class="m-0 w-40 overflow-hidden origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg outline-1 outline-black/5 transition transition-discrete [--anchor-gap:--spacing(2)] data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in dark:divide-white/10 dark:bg-gray-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10" id="menu-0" role="menu" open="" aria-labelledby="menu-button-1" tabindex="0">
             <slot name="content-dropdown"></slot>
         </el-menu>
  
@@ -16,11 +16,11 @@
 
 </template>
 
-<style>
+<!-- <style>
 
 el-menu {
   --anchor-offset: -500px;
   --anchor-gap: 70px ;
   overflow: hidden;
 }
-</style>
+</style> -->
